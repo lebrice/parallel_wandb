c8e53fdd450d9d3dbceba503d8f1f6089389ef53
Fabrice Normandin <fabrice.normandin@gmail.com>
2025-06-02 19:12:47 -0400

    Add test for calling log in scan and vmap(scan)
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- parallel_wandb/log_test.py
+++ parallel_wandb/log_test.py
@@ -1,10 +1,19 @@
+import functools
+import time
 from unittest.mock import Mock
 
 import numpy as np
+import optree
 import pytest
 from wandb.sdk.wandb_run import Run
+from pathlib import Path
+from parallel_wandb.init import wandb_init
+from parallel_wandb.utils import NestedSequence
 
 from .log import wandb_log
+
+import jax
+import jax.numpy as jnp
 
 
 def mock_run():
@@ -89,3 +98,71 @@ def test_wandb_log_with_vmap():
     fake_runs[0].log.assert_called_once_with({"a": jnp.array(0, dtype=jnp.int32)}, step=0)
     fake_runs[1].log.assert_called_once_with({"a": jnp.array(10, dtype=jnp.int32)}, step=10)
     fake_runs[2].log.assert_called_once_with({"a": jnp.array(20, dtype=jnp.int32)}, step=20)
+
+
+def training_step(
+    carry: jax.Array,
+    input: jax.Array,
+    *,
+    wandb_run: Run | NestedSequence[Run],
+    run_index: jax.Array | None = None,
+):
+    output = carry**2 + input
+    wandb_log(
+        wandb_run,
+        {"input": input, "output": output, "carry": carry},
+        step=carry,
+        run_index=run_index,
+    )
+    return carry + 1, output
+
+
+def test_log_in_scan(tmp_path: Path):
+    # Single run:
+    initial_value = 0
+    inputs = jnp.arange(5)
+    wandb_run = wandb_init(
+        {"config": {"initial_value": [initial_value]}},
+        project="test_project",
+        group="testing",
+        dir=tmp_path,
+        mode="offline",
+    )
+    assert isinstance(wandb_run, np.ndarray)
+    wandb_run = np.asanyarray(Mock(wraps=wandb_run.tolist()[0]))
+
+    final_value, output = jax.lax.scan(
+        functools.partial(training_step, wandb_run=wandb_run, run_index=jnp.arange(1)),
+        initial_value,
+        inputs,
+    )
+    jax.block_until_ready((final_value, output))
+    time.sleep(0.1)  # let teh callbacks run?
+    wandb_run.item().log.assert_called()
+
+
+def test_log_in_scan_with_vmap(tmp_path: Path):
+    initial_values = jnp.arange(3)
+    wandb_run = wandb_init(
+        {"config": {"initial_value": initial_values}},
+        project="test_project",
+        group="testing",
+        mode="offline",
+        dir=tmp_path,
+    )
+    assert isinstance(wandb_run, np.ndarray)
+    wandb_run = tuple(wandb_run.tolist())
+    wandb_run = optree.tree_map(lambda r: Mock(wraps=r), wandb_run)
+    inputs = jnp.arange(5)
+    final_values, outputs = jax.lax.scan(
+        jax.vmap(
+            functools.partial(
+                training_step, wandb_run=wandb_run, run_index=jnp.arange(len(initial_values))
+            ),
+            in_axes=(0, None),
+        ),
+        initial_values,
+        inputs,
+    )
+    jax.block_until_ready((final_values, outputs))
+    assert False, [wandb_run[i].log.mock_calls for i in range(len(wandb_run))]
